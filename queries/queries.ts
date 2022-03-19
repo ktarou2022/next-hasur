f5f07ffda6a202b89066e7523df39f92f9dfa079
ktarou2022 <94906157 +ktarou2022@users.noreply.github.com>
2022-03-20 00:22:35 +0900

    hasura crud

--- queries/queries.ts
+++ queries/queries.ts
@@ -48,7 +48,7 @@ export const CREATE_USER = gql`
 `
 export const UPDATE_USER = gql`
   mutation UpdateUser($id: uuid!, $name: String) {
-    update_users_by_pk(pk_columns: {id: "$id"}, _set: {name: $name}) {
+    update_users_by_pk(pk_columns: {id: $id}, _set: {name: $name}) {
       id
       name
       created_at
